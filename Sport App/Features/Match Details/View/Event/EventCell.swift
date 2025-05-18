cd89b9ad9f821cae14017f6e1d3c22317e63673a
Eslam Elsayed <eslimelseyd@gmail.com>
2025-05-18 23:53:18 +0300

    new development

---
+++ Sport App/Features/Match Details/View/Event/EventCell.swift
@@ -0,0 +1,28 @@
+//
+//  EventCell.swift
+//  Sport App
+//
+//  Created by Macos on 15/05/2025.
+//
+
+import UIKit
+
+class EventCell: UICollectionViewCell {
+
+    @IBOutlet weak var team1Img: UIImageView!
+    @IBOutlet weak var team2Img: UIImageView!
+    @IBOutlet weak var matchScore: UILabel!
+    @IBOutlet weak var team2: UILabel!
+    @IBOutlet weak var team1: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Initialization code
+        self.backgroundColor = UIColor(red: 43/255, green: 43/255, blue: 61/255, alpha: 1.0)
+
+                self.layer.cornerRadius = 20
+                self.layer.masksToBounds = true
+    }
+
+}
