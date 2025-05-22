64a7f53077fcf53c4741d8097f674db4109a794f
nada gharib <106126824+nada263204@users.noreply.github.com>
2025-05-22 22:56:15 +0300

    finish testing

---
+++ Sport App/Shared/NetworkRequesting.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkRequesting.swift
+//  Sport App
+//
+//  Created by Macos on 22/05/2025.
+//
+
+import Foundation
---
+++ Sport AppTests/MockNetworkRequester.swift
@@ -0,0 +1,8 @@
+//
+//  MockNetworkRequester.swift
+//  Sport AppTests
+//
+//  Created by Macos on 22/05/2025.
+//
+
+import Foundation
