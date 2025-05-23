e52a61f999467bcffbf5b32884a118278cc0eada
Eslam Elsayed <eslimelseyd@gmail.com>
2025-05-23 13:54:25 +0300

    search league

---
+++ Sport App/Features/Leagues/Views/HeaderSearchCollectionReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderSearchCollectionReusableView.swift
+//  Sport App
+//
+//  Created by Macos on 23/05/2025.
+//
+
+import UIKit
+
+class HeaderSearchCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
